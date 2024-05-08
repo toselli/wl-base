f63685d0e55894d1d1e362c6986de737b6edbe5c
ivanpuglia <ivanpuglia@gmail.com>
2024-05-08 17:58:21 -0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>
-    <div class="hero d-flex align-center">
-      <CommonSearch class="mt-3" alignTabs="left"  />
-    </div>
-  </div>
+    <CommonSearch class="mt-3" alignTabs="left"  />
+  <v-container class="content-container">
+      <v-row>
+        <v-col cols="12">
+          <HomeNews />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <HomeToursOnHome />
+        </v-col>
+      </v-row>
+        <v-row>
+          <v-col cols="12" lg="4">
+            <HomeExchanges />
+          </v-col>
+          <v-col>
+            <HomeEmergencyLines />
+          </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
