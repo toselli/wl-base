5eef16d0781a4b39c7002b14182ba19fd91c6a7f
ivanpuglia <ivanpuglia@gmail.com>
2024-05-11 19:34:05 -0300

    dayjs

--- plugins/dayjs.ts
+++ plugins/dayjs.ts
@@ -1,10 +1,10 @@
-// import dayjs from 'dayjs';
-// import relativeTime from 'dayjs/plugin/relativeTime';
-// import 'dayjs/locale/es';
+import dayjs from 'dayjs';
+//import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/es';
 
-// dayjs.extend(relativeTime);
-// dayjs.locale('es');
+//dayjs.extend(relativeTime);
+dayjs.locale('es');
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.provide('dayjs', dayjs); // Aunque es redundante si quieres seguir usando provide
-// });
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide('dayjs', dayjs); // Aunque es redundante si quieres seguir usando provide
+});
