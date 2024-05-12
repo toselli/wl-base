d935d786abf58f0061b99f8b4387c1161182ea18
ivanpuglia <ivanpuglia@gmail.com>
2024-05-12 00:14:07 -0300

    CASL

--- plugins/casl.ts
+++ plugins/casl.ts
@@ -2,7 +2,7 @@ import { ability } from "../plugins/ability";
 //import { abilitiesPlugin } from "@casl/vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use( ability);
+  nuxtApp.vueApp.use(ability);
 
   nuxtApp.provide('ability', ability);
 });
