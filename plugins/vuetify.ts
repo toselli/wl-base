dfa53d85fce035978446dc87aacadd96ed9cab58
Iván Puglia <ivanpuglia@gmail.com>
2024-05-13 12:32:14 -0300

    Update vuetify.ts

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,6 +1,4 @@
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import { md } from 'vuetify/lib/iconsets/md'
 import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'
@@ -10,10 +8,6 @@ import {ThemeLight, ThemeDark} from '~/custom/themes.js'
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: true,
-    components: {
-      ...components,
-    },
-    directives,
     icons: {
       defaultSet: 'mdi',
       aliases,
