dfa5801f373b8514654d06218790363dce5ec9fd
ivanpuglia <ivanpuglia@gmail.com>
2024-05-14 09:33:39 -0300

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <transition name="fade" v-if="showHideSpinner">
+      <AppLoadSpinner />
+    </transition>
+    <transition name="fade" v-else>
+      <div class="d-flex flex-column fill-height justify-space-start">
+        <AppHeader />
+        <VitePwaManifest />
+        <NuxtPage  />
+      </div>
+    </transition>
+    <AppFooter />
+    <AuthLoginDialog v-model="loginDialog" />
+  </v-app>
+</template>
+
+<script setup>
+const { $pwa } = useNuxtApp()
+const loginDialog = ref(false)
+
+const store = useLoginStore();
+
+const showHideSpinner = ref(true)
+
+onMounted(() => {
+  if ($pwa.offlineReady) {
+    console.log('App ready to work offline')
+  }
+  showHideSpinner.value = false;
+})
+
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
