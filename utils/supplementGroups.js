29cb28a9c9cf290ff20a36525a02d05ad7ef52ae
ivanpuglia <ivanpuglia@gmail.com>
2024-06-24 13:14:35 -0300

    utils

---
+++ utils/supplementGroups.js
@@ -0,0 +1,56 @@
+export const supplementGroups = {
+    'Desayuno': {
+        priority: 5, items: [
+            'Desayuno', 'Breakfast', 'American breakfast', 'Alojam. y Desayuno', 'ALOJAMIENTO', 'ALOJAMIENTO Y DESAYUNO', 'Alojamiento & Desayuno', 'Alojamiento y desayuno', 'Cama y desayuno',
+            'Alojamiento &amp; Desayuno', 'Alojamiento + Desayuno Americano', 'Alojamiento y Desayuno', 'Alojamiento y Desayuno Americano +', 'Desayuno incluído',
+            'Alojamiento, desayuno e impuestos', 'Always ME Courtyard Views-Bed & Breakfast-Refundable Rate', 'Apollon-Bed & Breakfast-Refundable Rate',
+            'Aura-Bed & Breakfast-Refundable Rate', 'Basic-Bed & Breakfast-Refundable Rate', 'BB', 'BB2', 'Bed & Breakfast', 'Bed &amp; Breakfast',
+            'Bed and breakfast', 'Bed and Breakfast', 'Bed and breakfast', 'Bed and breakfast ', 'Bed and Breakfast for 2 guests', 'Bed and Breakfast for 3 guests', 'Bed and English Breakfast',
+            'Bed&amp;Breakfast', 'Bed&amp;Breakfast (Non-Alcoholic)', 'Bed&Breakfast', 'Bed, breakfast and taxes', 'bed_breakfast', 'BedBreakfast', 'BREAKFAST',
+            'Breakfast For 1', 'BREAKFAST FOR 1 PERSON', 'BREAKFAST FOR 1 PERSON,COFFEE & TEA,EXPRESS CHECK-', 'Breakfast For 2', 'Breakfast for 2 guests', 'Free Breakfast', 'Free WiFi Free breakfast',
+            'BREAKFAST FOR 2 PEOPLE', 'BREAKFAST FOR 2 PEOPLE,FREE WIFI', 'Breakfast for two guests', 'Breakfast Included', 'Breakfast is included in the rates',
+            'Complimentary Breakfast', 'Continental breakfast', 'Continental breakfast for 2', 'Continental breakfast,Free WiFi', 'Continental Plan',
+            'Cosy Barcelona-Bed & Breakfast-Refundable Rate', 'Cosy-Bed & Breakfast-Refundable Rate', 'Courtyard-Bed & Breakfast-Refundable Rate',
+            'Daily Breakfast Included.', 'Deluxe Mountain View-Bed & Breakfast-Refundable Rate', 'Deluxe with Balcony-Bed & Breakfast-Refundable Rate',
+            'Deluxe-Bed & Breakfast-Refundable Rate', 'DESAYUNO', 'Desayuno americano', 'Desayuno Americano Complementario', 'Desayuno Americano Complementario para 2 pax',
+            'Desayuno Complementario', 'Desayuno continental', 'Desayuno Continental Complementario', 'Desayuno gratis', 'Desayuno incluÃ­do', 'Desayuno Incluido',
+            'Desayuno InglÃ©s', 'DESAYUNO INGLES', 'DESAYUNO LIGERO', 'Desayuno para 1 persona', 'Desayuno para 2', 'Desayuno para 2 personas', 'Desayuno para dos',
+            'Desayuno y cena incluidos', 'Economy-Bed & Breakfast-Refundable Rate', 'English breakfast', 'English Buffet Breakfast', 'With Breakfast', 'Bed and Breakfast for 2', 'Pernottamento e Prima Colazione',
+            'Desayuno bufet', 'Desayuno buffet', 'Desayuno buffet americano en el hotel', 'Desayuno Buffet en el hotel', 'Desayuno Buffet Hot Complementario',
+            'bb- breakfast buffet', 'Breakfast Buffet', 'Bed and breakfast', 'Breakfast buffet,Free WiFi', 'Desayuno estilo bufÃ©'
+        ]
+    },
+    'Desayuno y cena': {
+        priority: 4, items: [
+            'Breakfast & Dinner', 'Desayuno y cena incluidos' , 'Free Dinner'
+        ]
+    },
+    'Sólo habitación': {
+        priority: 6, items: [
+            'Sólo habitación', 'Free Wifi', 'No Board', 'Sólo Habitación', 'Solo Habitacion'  , 'Solo Alojamiento', 'ROOM(ONLY)', 'Sólo Alojamiento', 'Sólo alojamiento', 'Solo Room', 'Room Only', 'Room only', 'Alojamiento con cocina', 'Economy-Room Only-Refundable Rate', 'EP-NO MEALS', 'SOLO HABITACIÓN'
+        ]
+    },
+    'All Inclusive': {
+        priority: 1, items: [
+            'Todo incluido', 'ALL - INCLUSIVE', 'All inclusive' , 'All inclusive ', 'Todo incluído', 'All included', 'All Inclusive', 'All Inclusive (Tudo IncluÃ­do)', 'All Inclusive for 4 guests',
+            'All Inclusive light', 'All Inclusive Platinum', 'All Inclusive: All Inclusive', 'All Meals', 'AllInclusive', 'All-Inclusive',
+            'All-Inclusive (Food/Beverages/Snacks)', 'All-inclusive,Free WiFi', 'Double-All Inclusive-Refundable Rate', 'Todo Incluido', 'Todo incluido Soft .',
+            'Todo Incluido.', 'Tudo IncluÃ­do', 'All inclusive (All-Inclusive)'
+        ]
+    },
+    'Media pensión': {
+        priority: 2, items: [
+            'Media pensión', 'Half board', 'Alojamiento y PAM (Desayuno y Almuerzo o Cena)'
+        ]
+    },
+    'Pensión completa': {
+        priority: 3, items: [
+            'Full board', 'Pensión completa', 'Full Pension', 'Full pension'
+        ]
+    },
+    'Otros': {
+        priority: 7, items: [
+            'Aparcamiento', 'Express check-in', 'Wifi gratis'
+        ]
+    },
+};
